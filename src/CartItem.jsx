73571b0564fe4737021fd8c87428f72cf53b02ff
ngnepist_sabi_agri <stephane.ngnepiepaye-wembe@sabi-agri.com>
2025-01-13 14:47:51 +0100

    plant shopping

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,37 +1,74 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, updateAddedTocart}) => {
   const cart = useSelector(state => state.cart.items);
+  
   const dispatch = useDispatch();
-
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+      const cleanedCost = item.cost.replace("$", ""); // Remove the dollar sign
+      const costNumber = parseFloat(cleanedCost);
+      totalCost += costNumber * item.quantity;});
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    const updatedItem = { ...item, quantity: item.quantity + 1 }; // Create a new object
+    dispatch(updateQuantity(updatedItem));
   };
 
   const handleDecrement = (item) => {
-   
+    const updatedItem = { ...item, quantity: item.quantity - 1 }; // Create a new object
+    if (updatedItem.quantity > 0) {
+      dispatch(updateQuantity(updatedItem));
+    }
+    else
+    {
+      dispatch(removeItem(item));
+      updateAddedTocart((prevState) => {
+        // Create a new object without the specified key
+        const newObject = Object.fromEntries(
+          Object.entries(prevState).filter(([key]) => key !== item.name)
+        );
+        return newObject;
+      });
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
+    updateAddedTocart((prevState) => {
+              // Create a new object without the specified key
+              const newObject = Object.fromEntries(
+                Object.entries(prevState).filter(([key]) => key !== item.name)
+            );
+            return newObject;
+  });
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cleanedCost = item.cost.replace("$", ""); // Remove the dollar sign
+    const costNumber = parseFloat(cleanedCost);
+    let total = costNumber * item.quantity
+    return total;
   };
 
+  
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -57,7 +94,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
